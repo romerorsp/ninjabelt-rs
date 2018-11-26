4b937a62d37f245c54182764b2364e6109d7f569
Romero Ricardo @ CINQ <r.sousa@cinq.com.br>
2018-11-26 19:34:56 -0200

    Media com a patroa

--- src/main/java/br/com/cinq/dojo/ninjabelt/rs/controller/GreetingController.java
+++ src/main/java/br/com/cinq/dojo/ninjabelt/rs/controller/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Shauzinha linda") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
